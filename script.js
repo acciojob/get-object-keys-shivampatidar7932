76766995fd94f84a2a520c93710d9cd16e4f012e
Shivam Patidar <125869590+shivampatidar7932@users.noreply.github.com>
2023-12-27 17:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Creating an object called student with a property called name
+var student = {
+  name: "John Doe"
+};
+
+// Adding a property to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
+
+// Testing the getKeys() method on the student object
+var keysArray = student.getKeys();
+console.log(keysArray); // Output: ['name']
